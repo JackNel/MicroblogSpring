462aeda6be4f41e7c2b708bab65b6a2e40cff848
JackNel <jcneligan@gmail.com>
2015-11-09 19:07:59 -0500

    Add/Delete Messages

---
+++ src/main/java/com/theironyard/Message.java
@@ -0,0 +1,10 @@
+package com.theironyard;
+
+/**
+ * Created by Jack on 11/9/15.
+ */
+public class Message {
+    int id;
+    String text;
+
+}
--- src/main/java/com/theironyard/MicroblogSpringController.java
+++ src/main/java/com/theironyard/MicroblogSpringController.java
@@ -1,20 +1,53 @@
 package com.theironyard;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 /**
  * Created by Jack on 11/9/15.
  */
+
+@Controller
 public class MicroblogSpringController {
+    ArrayList<Message> messages = new ArrayList();
+
     @RequestMapping("/")
     public String home(Model model, HttpServletRequest request) {
         HttpSession session = request.getSession();
         String username = (String) session.getAttribute("username");
         model.addAttribute("username", username);
+        model.addAttribute("messages", messages);
         return "home";
+    }
+
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request, String username) {
+        HttpSession session = request.getSession();
+        session.setAttribute("username", username);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/add-message")
+    public String message(String text) {
+        Message message = new Message();
+        message.id = messages.size() + 1;
+        message.text = text;
+        messages.add(message);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/delete-message")
+    public String delete(Integer id) {
+        messages.remove(id-1);
+        for (int i = 0; i < messages.size(); i++) {
+            messages.get(i).id = i + 1;
+        }
+        return "redirect:/";
     }
 }
